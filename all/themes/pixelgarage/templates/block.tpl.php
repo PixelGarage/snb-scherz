2785afcd0c7f7598d3dd7b9002e0cb89fc2d6eed
Ralph Moser <ralph@ramosoft.ch>
2017-03-20 01:12:21 +0100

    - first Version Core -> 7.54

---
+++ all/themes/pixelgarage/templates/block.tpl.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display a block.
+ *
+ * Available variables:
+ * - $block->subject: Block title.
+ * - $content: Block content.
+ * - $block->module: Module that generated the block.
+ * - $block->delta: An ID for the block, unique within each module.
+ * - $block->region: The block region embedding the current block.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - block: The current template type, i.e., "theming hook".
+ *   - block-[module]: The module generating the block. For example, the user
+ *     module is responsible for handling the default user navigation block. In
+ *     that case the class would be 'block-user'.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * Helper variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $block_zebra: Outputs 'odd' and 'even' dependent on each block region.
+ * - $zebra: Same output as $block_zebra but independent of any block region.
+ * - $block_id: Counter dependent on each block region.
+ * - $id: Same output as $block_id but independent of any block region.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ * - $block_html_id: A valid HTML ID and guaranteed unique.
+ *
+ * @see bootstrap_preprocess_block()
+ * @see template_preprocess()
+ * @see template_preprocess_block()
+ * @see bootstrap_process_block()
+ * @see template_process()
+ *
+ * @ingroup templates
+ */
+// node to load
+$nid = 21;
+$node = node_load($nid);
+$rendered_item = node_view($node, 'default');
+
+// modal dialog
+$path = drupal_get_path('theme', 'pixelgarage') . '/images/popup_close.svg';
+$button_text = t('Hier anmelden');
+$modal_title = null;
+$close_text = t('Schliessen');
+$close_img = file_create_url($path);
+
+?>
+<section id="block-registration" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <?php print $content ?>
+  <!-- modal trigger -->
+  <a class="button" role="button" data-toggle="modal" data-target="#snb-modal">
+    <?php print $button_text; ?>
+  </a>
+</section>
+
+<div id="snb-modal" class="modal" tabindex="-1" role="dialog" aria-labelledby="snb-modal-label" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <!-- Header -->
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><img src="<?php print $close_img ?>" aria-hidden="true"/></button>
+        <?php if ($modal_title): ?>
+          <h2 class="modal-title" id="snb-modal-label"><?php print $modal_title; ?></h2>
+        <?php endif; ?>
+        <div class="body-fading body-fading-top"></div>
+      </div>
+      <!-- Body -->
+      <div class="modal-body">
+        <?php if ($rendered_item) print render($rendered_item) ; ?>
+      </div>
+      <!-- Footer -->
+      <div class="modal-footer">
+        <div class="body-fading body-fading-bottom"></div>
+        <button type="button" class="btn btn-default" data-dismiss="modal"><?php print $close_text; ?></button>
+      </div>
+
+    </div>
+  </div>
+</div>
